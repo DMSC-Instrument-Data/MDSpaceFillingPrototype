69563a0ecd5d3307014571b97076287c067bfcad
Dan Nixon <dan@dan-nixon.com>
2018-08-02 11:57:51 +0100

    Use template specializations for pad and compact functions

--- src/BitInterleaving.h
+++ src/BitInterleaving.h
@@ -3,11 +3,19 @@
 
 #pragma once
 
+template <size_t N, typename Int> Int pad(Int x) {
+  throw std::runtime_error("");
+}
+
+template <size_t N, typename Int> Int compact(Int x) {
+  throw std::runtime_error("");
+}
+
 /**
  * Pad an integer with 1 padding bit between integer bits.
  * Maximum input width is 32 bit.
  */
-uint64_t Pad1_64(uint64_t x) {
+template <> uint64_t pad<1, uint64_t>(uint64_t x) {
   x &= 0xffff;
   x = (x | x << 8) & 0xff00ff;
   x = (x | x << 4) & 0xf0f0f0f;
@@ -21,7 +29,7 @@ uint64_t Pad1_64(uint64_t x) {
  * bits.
  * Maximum output bit width is 32 bit.
  */
-uint64_t Compact1_64(uint64_t x) {
+template <> uint64_t compact<1, uint64_t>(uint64_t x) {
   x &= 0x55555555;
   x = (x | x >> 1) & 0x33333333;
   x = (x | x >> 2) & 0xf0f0f0f;
@@ -34,7 +42,7 @@ uint64_t Compact1_64(uint64_t x) {
  * Pad an integer with 2 padding bits between integer bits.
  * Maximum input width is 16 bit.
  */
-uint64_t Pad2_64(uint64_t x) {
+template <> uint64_t pad<2, uint64_t>(uint64_t x) {
   x &= 0xffff;
   x = (x | x << 16) & 0xff0000ff;
   x = (x | x << 8) & 0xf00f00f00f;
@@ -48,7 +56,7 @@ uint64_t Pad2_64(uint64_t x) {
  * bits.
  * Maximum output bit width is 16 bit.
  */
-uint64_t Compact2_64(uint64_t x) {
+template <> uint64_t compact<2, uint64_t>(uint64_t x) {
   x &= 0x249249249249;
   x = (x | x >> 2) & 0xc30c30c30c3;
   x = (x | x >> 4) & 0xf00f00f00f;
@@ -61,7 +69,7 @@ uint64_t Compact2_64(uint64_t x) {
  * Pad an integer with 3 padding bits between integer bits.
  * Maximum input width is 16 bit.
  */
-uint64_t Pad3_64(uint64_t x) {
+template <> uint64_t pad<3, uint64_t>(uint64_t x) {
   x &= 0xffff;
   x = (x | x << 32) & 0xf800000007ff;
   x = (x | x << 16) & 0xf80007c0003f;
@@ -77,7 +85,7 @@ uint64_t Pad3_64(uint64_t x) {
  * bits.
  * Maximum output bit width is 16 bit.
  */
-uint64_t Compact3_64(uint64_t x) {
+template <> uint64_t compact<3, uint64_t>(uint64_t x) {
   x &= 0x1111111111111111;
   x = (x | x >> 1) & 0x909090909090909;
   x = (x | x >> 2) & 0x843084308430843;
@@ -92,38 +100,43 @@ uint64_t Compact3_64(uint64_t x) {
  * Interleaves two 16 bit integers into a single 32 bit integer.
  */
 uint32_t Interleave_2_16_32(uint32_t a, uint32_t b) {
-  return Pad1_64(a) | (Pad1_64(b) << 1);
+  const auto padFunc = pad<1, uint64_t>;
+  return padFunc(a) | (padFunc(b) << 1);
 }
 
 /**
  * Deinterleaves a 32 bit integer into two 16 bit integers.
  */
 void Deinterleave_2_16_32(uint32_t z, uint16_t &a, uint16_t &b) {
-  a = Compact1_64(z);
-  b = Compact1_64(z >> 1);
+  const auto compactFunc = compact<1, uint64_t>;
+  a = compactFunc(z);
+  b = compactFunc(z >> 1);
 }
 
 /**
  * Interleaves three 16 bit integers into a single 64 bit integer.
  */
 uint64_t Interleave_3_16_64(uint64_t a, uint64_t b, uint64_t c) {
-  return Pad2_64(a) | (Pad2_64(b) << 1) | (Pad2_64(c) << 2);
+  const auto padFunc = pad<2, uint64_t>;
+  return padFunc(a) | (padFunc(b) << 1) | (padFunc(c) << 2);
 }
 
 /**
  * Deinterleaves a 64 bit integer into three 16 bit integers.
  */
 void Deinterleave_3_16_64(uint64_t z, uint16_t &a, uint16_t &b, uint16_t &c) {
-  a = Compact2_64(z);
-  b = Compact2_64(z >> 1);
-  c = Compact2_64(z >> 2);
+  const auto compactFunc = compact<2, uint64_t>;
+  a = compactFunc(z);
+  b = compactFunc(z >> 1);
+  c = compactFunc(z >> 2);
 }
 
 /**
  * Interleaves four 16 bit integers into a single 64 bit integer.
  */
 uint64_t Interleave_4_16_64(uint64_t a, uint64_t b, uint64_t c, uint64_t d) {
-  return Pad3_64(a) | (Pad3_64(b) << 1) | (Pad3_64(c) << 2) | (Pad3_64(d) << 3);
+  const auto padFunc = pad<3, uint64_t>;
+  return padFunc(a) | (padFunc(b) << 1) | (padFunc(c) << 2) | (padFunc(d) << 3);
 }
 
 /**
@@ -131,8 +144,9 @@ uint64_t Interleave_4_16_64(uint64_t a, uint64_t b, uint64_t c, uint64_t d) {
  */
 void Deinterleave_4_16_64(uint64_t z, uint16_t &a, uint16_t &b, uint16_t &c,
                           uint16_t &d) {
-  a = Compact3_64(z);
-  b = Compact3_64(z >> 1);
-  c = Compact3_64(z >> 2);
-  d = Compact3_64(z >> 3);
+  const auto compactFunc = compact<3, uint64_t>;
+  a = compactFunc(z);
+  b = compactFunc(z >> 1);
+  c = compactFunc(z >> 2);
+  d = compactFunc(z >> 3);
 }
