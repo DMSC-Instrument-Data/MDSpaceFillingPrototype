d42fc59698424230c63af5713ba90a5c4ba202d5
Dan Nixon <dan@dan-nixon.com>
2018-08-13 13:46:29 +0100

    Test coordinates for 2D box example

--- src/test/MDBox2DTest.cpp
+++ src/test/MDBox2DTest.cpp
@@ -52,5 +52,63 @@ TEST(MDBox2DTest, test_fill_events) {
     EXPECT_EQ(1, child.eventCount());
     EXPECT_EQ(curveIt, child.eventBegin());
     EXPECT_EQ(++curveIt, child.eventEnd());
+
+    /* Ensure child has correct Morton "size"/"width" */
+    EXPECT_EQ(std::numeric_limits<MortonT>::max() / 4,
+              child.max() - child.min());
+  }
+
+  /* Positions in brackets assume axis origin in top left of coordinate space */
+
+  /* Test axis dimensions of box 0 (upper left) */
+  {
+    IntArray<ND, IntT> lower =
+        deinterleave<ND, IntT, MortonT>(root.children()[0].min());
+    EXPECT_EQ(0, lower[0]);
+    EXPECT_EQ(0, lower[1]);
+
+    IntArray<ND, IntT> upper =
+        deinterleave<ND, IntT, MortonT>(root.children()[0].max());
+    EXPECT_EQ(32767, upper[0]);
+    EXPECT_EQ(32767, upper[1]);
+  }
+
+  /* Test axis dimensions of box 1 (upper right) */
+  {
+    IntArray<ND, IntT> lower =
+        deinterleave<ND, IntT, MortonT>(root.children()[1].min());
+    EXPECT_EQ(32768, lower[0]);
+    EXPECT_EQ(0, lower[1]);
+
+    IntArray<ND, IntT> upper =
+        deinterleave<ND, IntT, MortonT>(root.children()[1].max());
+    EXPECT_EQ(65535, upper[0]);
+    EXPECT_EQ(32767, upper[1]);
+  }
+
+  /* Test axis dimensions of box 2 (lower left) */
+  {
+    IntArray<ND, IntT> lower =
+        deinterleave<ND, IntT, MortonT>(root.children()[2].min());
+    EXPECT_EQ(0, lower[0]);
+    EXPECT_EQ(32768, lower[1]);
+
+    IntArray<ND, IntT> upper =
+        deinterleave<ND, IntT, MortonT>(root.children()[2].max());
+    EXPECT_EQ(32767, upper[0]);
+    EXPECT_EQ(65535, upper[1]);
+  }
+
+  /* Test axis dimensions of box 3 (lower right) */
+  {
+    IntArray<ND, IntT> lower =
+        deinterleave<ND, IntT, MortonT>(root.children()[3].min());
+    EXPECT_EQ(32768, lower[0]);
+    EXPECT_EQ(32768, lower[1]);
+
+    IntArray<ND, IntT> upper =
+        deinterleave<ND, IntT, MortonT>(root.children()[3].max());
+    EXPECT_EQ(65535, upper[0]);
+    EXPECT_EQ(65535, upper[1]);
   }
 }
