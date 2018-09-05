bc33044a78f1296d2674b79b2e57fe888bfcbdc5
Dan Nixon <dan@dan-nixon.com>
2018-09-05 15:18:19 +0100

    Add status output to bit padding generator script

--- docs/bit_padding_generator.py
+++ docs/bit_padding_generator.py
@@ -4,6 +4,8 @@
 # Tidied up and stripped down version of the script listed here: https://stackoverflow.com/a/18528775
 
 def generate_bit_spacing_code(bit_count, empty_bit_count):
+    print('Pad {} bits with {} padding bits'.format(bit_count, empty_bit_count))
+
     bit_distances = [bin(i * empty_bit_count)[2:] for i in range(bit_count)]
     move_bits = []
 
