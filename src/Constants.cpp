b52f251192016503e596da14a6ca3553565da6ef
Dan Nixon <dan@dan-nixon.com>
2018-10-16 13:28:46 +0100

    Add missing morton mask

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -20,23 +20,44 @@
 
 #include <cinttypes>
 
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace boost::multiprecision::literals;
+
 template <>
 const uint32_t MortonMask<2, uint16_t, uint32_t>::mask[] = {
-    0x55555555, 0xaaaaaaaa,
+    0x55555555,
+    0xaaaaaaaa,
 };
 
 template <>
 const uint64_t MortonMask<3, uint16_t, uint64_t>::mask[] = {
-    0x249249249249, 0x492492492492, 0x924924924924,
+    0x249249249249,
+    0x492492492492,
+    0x924924924924,
 };
 
 template <>
 const uint32_t MortonMask<4, uint8_t, uint32_t>::mask[] = {
-    0x11111111, 0x22222222, 0x44444444, 0x88888888,
+    0x11111111,
+    0x22222222,
+    0x44444444,
+    0x88888888,
 };
 
 template <>
 const uint64_t MortonMask<4, uint16_t, uint64_t>::mask[] = {
-    0x1111111111111111, 0x2222222222222222, 0x4444444444444444,
+    0x1111111111111111,
+    0x2222222222222222,
+    0x4444444444444444,
     0x8888888888888888,
+};
+
+template <>
+const boost::multiprecision::uint128_t
+    MortonMask<4, uint32_t, boost::multiprecision::uint128_t>::mask[] = {
+        0x11111111111111111111111111111111_cppui128,
+        0x22222222222222222222222222222222_cppui128,
+        0x44444444444444444444444444444444_cppui128,
+        0x88888888888888888888888888888888_cppui128,
 };
