c75e44c27828f8c7e48d3172564c43cf8e09bb2d
Igor Gudich <igor.gudich@esss.se>
2018-11-06 10:00:20 +0100

    Create appendEvents function function for STORING_EVENTS mode

--- src/test/AppendEventsTest.cpp
+++ src/test/AppendEventsTest.cpp
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 #include <gtest/gtest.h>
 
 #include <limits>
@@ -25,6 +26,9 @@
 #include "MDBox.h"
 #include "MDEvent.h"
 
+#ifdef STORING_EVENTS
+
+
 constexpr size_t ND(4);
 using IntT = uint16_t;
 using MortonT = uint64_t;
@@ -33,79 +37,9 @@ constexpr auto interleaveFunc = interleave<ND, IntT, MortonT>;
 using Event = MDEvent<ND, IntT, MortonT>;
 using Box = MDBox<ND, IntT, MortonT>;
 
-TEST(MDBoxTest, test_simple) {
-  /* Dummy event list required to pass iterators to MDBox constructor */
-  std::vector<Event> dummyEventList;
-
-  Box root(dummyEventList.cbegin(), dummyEventList.cend());
-
-  /* Should be full integer range */
-  using intLimits = std::numeric_limits<MortonT>;
-  EXPECT_EQ(intLimits::min(), root.min());
-  EXPECT_EQ(intLimits::max(), root.max());
-
-  /* Should not have children */
-  EXPECT_EQ(0, root.children().size());
-
-  /* Should not have any events */
-  EXPECT_EQ(0, root.eventCount());
-}
-
-TEST(MDBox4DTest, test_contains) {
-  /* Dummy event list required to pass iterators to MDBox constructor */
-  std::vector<Event> dummyEventList;
-
-  /* Bounds of box (same for all axes) */
-  constexpr IntT lower(5);
-  constexpr IntT upper(8);
-
-  /* One coordinate value outside of bounds */
-  constexpr auto oneLower(lower - 1);
-  constexpr auto oneUpper(upper + 1);
-
-  /* A coordinate value that is inside the bounds */
-  constexpr IntT inside(7);
-
-  Box box(dummyEventList.cbegin(), dummyEventList.cend(),
-          interleaveFunc({lower, lower, lower, lower}),
-          interleaveFunc({upper, upper, upper, upper}));
-
-  /* Event is inside box */
-  EXPECT_TRUE(
-      box.contains(Event(interleaveFunc({lower, lower, lower, lower}))));
-  EXPECT_TRUE(
-      box.contains(Event(interleaveFunc({inside, inside, inside, inside}))));
-  EXPECT_TRUE(
-      box.contains(Event(interleaveFunc({upper, upper, upper, upper}))));
-
-  /* Event Morton number is lesser than box boundary */
-  EXPECT_FALSE(box.contains(
-      Event(interleaveFunc({oneLower, oneLower, oneLower, oneLower}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({lower, lower, lower, oneLower}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({lower, lower, oneLower, lower}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({lower, oneLower, lower, lower}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({oneLower, lower, lower, lower}))));
-
-  /* Event Morton number is greater than box boundary */
-  EXPECT_FALSE(box.contains(
-      Event(interleaveFunc({oneUpper, oneUpper, oneUpper, oneUpper}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({upper, upper, upper, oneUpper}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({upper, upper, oneUpper, upper}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({upper, oneUpper, upper, upper}))));
-  EXPECT_FALSE(
-      box.contains(Event(interleaveFunc({oneUpper, upper, upper, upper}))));
-}
-
-TEST(MDBox4DTest, test_fill_events) {
+TEST(MDBox4DTest, test_append_events) {
   /* Create test MD event Z-curve */
-  Event::ZCurve curve;
+  Event::ZCurve curve, curveNew;
   {
     const IntT step = std::numeric_limits<IntT>::max() / 4;
     const IntT a(step);
@@ -125,14 +59,15 @@ TEST(MDBox4DTest, test_fill_events) {
     curve.emplace_back(interleaveFunc({b + 1, a, a, a}));
     curve.emplace_back(interleaveFunc({b + 2, a, a, a}));
     curve.emplace_back(interleaveFunc({b, a, a, b}));
-    curve.emplace_back(interleaveFunc({b, a, b, a}));
-    curve.emplace_back(interleaveFunc({b, a, b, b}));
-    curve.emplace_back(interleaveFunc({b, b, a, a}));
-    curve.emplace_back(interleaveFunc({b, b, a, b}));
-    curve.emplace_back(interleaveFunc({b, b, b, a}));
-    curve.emplace_back(interleaveFunc({b, b, b, b}));
+    curveNew.emplace_back(interleaveFunc({b, a, b, a}));
+    curveNew.emplace_back(interleaveFunc({b, a, b, b}));
+    curveNew.emplace_back(interleaveFunc({b, b, a, a}));
+    curveNew.emplace_back(interleaveFunc({b, b, a, b}));
+    curveNew.emplace_back(interleaveFunc({b, b, b, a}));
+    curveNew.emplace_back(interleaveFunc({b, b, b, b}));
 
     std::sort(curve.begin(), curve.end());
+    std::sort(curveNew.begin(), curveNew.end());
   }
 
   /* Create root box and set events */
@@ -141,13 +76,13 @@ TEST(MDBox4DTest, test_fill_events) {
   /* Distribute events, split when a box contains 2 or greater events, limit to
    * three levels of child boxes */
   root.distributeEvents(2, 4);
+  root.appendEvents(curveNew, 2, 4);
 
   /* Should contain all events */
-  EXPECT_EQ(20, root.eventCount());
-#ifndef STORING_EVENTS // lose iterators of initial vector if STORING_EVENTS
-  EXPECT_EQ(curve.cbegin(), root.eventBegin());
-  EXPECT_EQ(curve.cend(), root.eventEnd());
-#endif // STORING_EVENTS
+  EXPECT_EQ(20, root.totalEvents());
+
+
+
 
   /* Expected structure */
   ExpectedBox expectedRoot{20,
@@ -246,3 +181,5 @@ TEST(MDBox4DTest, test_fill_events) {
   Box::ZCurveIterator curveIt = curve.cbegin();
   recursive_box_tree_validation(root, curveIt, expectedRoot);
 }
+
+#endif // STORING_EVENTS
