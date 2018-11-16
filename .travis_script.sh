16ad393190c4b99922687b4b53aa0b9957661e08
Dan Nixon <dan@dan-nixon.com>
2018-11-16 09:40:58 +0000

    Ensure Travis build fails early

--- .travis_script.sh
+++ .travis_script.sh
@@ -6,8 +6,7 @@ conan remote add ess-dmsc https://api.bintray.com/conan/ess-dmsc/conan
 mkdir -p build
 cd build
 
-conan install ${HOME}/src/conanfile.txt -s build_type=Release -s compiler.libcxx=libstdc++11 --build missing
-cmake -DCMAKE_BUILD_TYPE=Release ${HOME}/src
-make -j4
-
+conan install ${HOME}/src/conanfile.txt -s build_type=Release -s compiler.libcxx=libstdc++11 --build missing && \
+cmake -DCMAKE_BUILD_TYPE=Release ${HOME}/src && \
+make -j`nproc` && \
 ctest -V -L UnitTest
